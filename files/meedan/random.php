8ced33f5ec0e86b080ec578a89943f3fb67e0342
Caio <117518+caiosba@users.noreply.github.com>
2023-07-27 13:18:53 -0300

    No need for the files/ directory anymore

--- files/meedan/random.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-  <head>
-    <title>Random: <?php print rand(1,100000); ?></title>
-  </head>
-  <body>
-    <p>Random: <?php print rand(1,100000); ?></p>
-  </body>
-</html>
--- files/others/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-sleep(10);
-echo 'Rendered after 10 seconds';
-?>
