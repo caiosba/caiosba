35adbbdf109917ae3815871c89c4ae18f938aee6
Caio <117518+caiosba@users.noreply.github.com>
2023-07-26 21:07:32 -0300

    Adding files for the website

---
+++ files/meedan/random.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Random: <?php print rand(1,100000); ?></title>
+  </head>
+  <body>
+    <p>Random: <?php print rand(1,100000); ?></p>
+  </body>
+</html>
---
+++ files/others/test.php
@@ -0,0 +1,4 @@
+<?php
+sleep(10);
+echo 'Rendered after 10 seconds';
+?>
